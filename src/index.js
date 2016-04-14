dd7b80cbef4411f82e16dd49544aac3fef888fb4
郭鹏 <guopengb@chanjet.com>
2016-04-14 14:35:57 +0800

    添加border

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ export default function ({ types: t }) {
                 exit({ node }) {
                     var valNode = node.value;
                     var keyNode = node.key;
-                    var isAllow = /^padding|^margin|width|height|lineHeight|fontSize/.test(keyNode.name||keyNode.value);
+                    var isAllow = /^padding|^margin|^border|^background|width|height|lineHeight|fontSize/i.test(keyNode.name||keyNode.value);
                     var val = valNode.value;
                     if (valNode.type ==='StringLiteral' && /px/.test(val) && isAllow) {
                         valNode.value = val.replace(/(\d+)px/g,function (px) {
